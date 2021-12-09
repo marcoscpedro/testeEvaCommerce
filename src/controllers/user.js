69a53f711e900584b92a4b73058558298cde00c9
Marcos Pedro <marcospedrocm@hotmail.com>
2021-12-08 21:17:55 -0300

    post User create

---
+++ src/controllers/user.js
@@ -0,0 +1,13 @@
+const User = require("../models/user")
+
+const create = async (req,res) => {
+    const newUser = User.create(req.body)
+    res.send(newUser)
+}
+
+
+
+
+module.exports = {
+    create()
+}
--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,22 @@
 const database = require("../config/db")
+const userTable = require("../tables/user")
+const md5 = require("md5")
 
 class User {
     static async create(Obj) {
-        // // const sql =
-        // await database.execute
+        const sql = `INSERT INTO users(name, email, password) VALUES(?,?,?)`
+        const values = [Obj.name, Obj.email, md5(Obj.password)]
+        try {
+            const user = await database.execute(sql,values)
+            return {
+                id:user[0].insertId,
+            }
+        } catch (err) {
+            return {
+                error: true,
+                message: err
+            }
+        }
     }
 }
 
